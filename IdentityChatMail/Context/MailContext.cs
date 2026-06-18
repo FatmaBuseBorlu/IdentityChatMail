7805e9704fdd5434f329b6178771f722b90b107a
Fatma Buse Borlu <fatmabuseborlu@gmail.com>
2026-06-18 23:00:44 +0300

    Remove hardcoded SQL Server name from DbContext

--- IdentityChatMail/Context/MailContext.cs
+++ IdentityChatMail/Context/MailContext.cs
@@ -1,15 +1,15 @@
-﻿using IdentityChatMail.Entities;
+using IdentityChatMail.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace IdentityChatMail.Context
 {
-    public class MailContext: IdentityDbContext<AppUser>
+    public class MailContext : IdentityDbContext<AppUser>
     {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        public MailContext(DbContextOptions<MailContext> options) : base(options)
         {
-            optionsBuilder.UseSqlServer("Server=DESKTOP-NBRMDOS; initial Catalog=EmailChatDb;integrated Security=true;trust server certificate=true");
         }
+
         public DbSet<Message> Messages { get; set; }
     }
 }
