27e56e3f75f04af7684ef8af0fae7e973b91a7c9
Fatma Buse Borlu <fatmabuseborlu@gmail.com>
2026-06-18 23:00:41 +0300

    Read SQL connection string from configuration

--- IdentityChatMail/Program.cs
+++ IdentityChatMail/Program.cs
@@ -2,22 +2,24 @@ using IdentityChatMail.Context;
 using IdentityChatMail.Entities;
 using IdentityChatMail.Models;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddDbContext<MailContext>();
-builder.Services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<MailContext>().AddErrorDescriber<CustomIdentityValidator>();
+builder.Services.AddDbContext<MailContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<AppUser, IdentityRole>()
+    .AddEntityFrameworkStores<MailContext>()
+    .AddErrorDescriber<CustomIdentityValidator>();
 
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
